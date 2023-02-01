63ab066c5e78f5348f5201ab58641680f40adef5
quangsang239 <quangsang239@gmail.com>
2023-02-01 15:34:44 +0700

    first commit

---
+++ generators/app/index.js
@@ -0,0 +1,41 @@
+import Generator from "yeoman-generator";
+import _ from "lodash";
+
+export default class extends Generator {
+  async prompting() {
+    const { typeApp, appName } = await this.prompt([
+      {
+        type: "list",
+        name: "typeApp",
+        message: "Select app?",
+        choices: ["ReactJS App", "VueJS App"],
+      },
+      {
+        type: "input",
+        name: "appName",
+        message: "Please enter the project name!",
+      },
+    ]);
+    this.typeApp = typeApp;
+    this.appName = appName;
+  }
+
+  writing() {
+    if (this.typeApp === "ReactJS App") this._generatorReactApp();
+    if (this.typeApp === "VueJS App") this._generatorVueApp();
+  }
+
+  _generatorReactApp() {
+    this.fs.copy(
+      this.templatePath("react-template"),
+      this.destinationPath(`${this.appName}`)
+    );
+  }
+
+  _generatorVueApp() {
+    this.fs.copy(
+      this.templatePath("vue-template"),
+      this.destinationPath(`${this.appName}`)
+    );
+  }
+}
